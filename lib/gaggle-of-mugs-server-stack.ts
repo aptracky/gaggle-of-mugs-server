12242c7dd3ea91d1c56d7c87f80c9e0486fb111a
Andrew Peterson <apeterson247@gmail.com>
2023-09-21 18:49:29 -0400

    new function

--- lib/gaggle-of-mugs-server-stack.ts
+++ lib/gaggle-of-mugs-server-stack.ts
@@ -1,13 +1,14 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
-import { Lambda } from "aws-cdk-lib/aws-ses-actions";
 import { Queue } from "aws-cdk-lib/aws-sqs";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export class GaggleOfMugsServerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Visit DLQ
     const visitDLQ = new Queue(this, "visitDLQ", {
       queueName: "visitDLQ",
       retentionPeriod: Duration.days(14),
@@ -26,6 +27,13 @@ export class GaggleOfMugsServerStack extends Stack {
     const visitTable = new Table(this, "visits", {
       partitionKey: { name: "visitId", type: AttributeType.STRING },
       sortKey: { name: "locationId", type: AttributeType.STRING },
+    });
+
+    const handler = new Function(this, "visitFunction", {
+      functionName: "visitFunction",
+      code: Code.fromAsset("src"),
+      handler: "visitFunction.handler",
+      runtime: Runtime.NODEJS_18_X,
     });
   }
 }
---
+++ src/visitFunction/index.ts
@@ -0,0 +1,5 @@
+const handler = (event: Event) => {
+  return { status: 200, message: "Hello World" };
+};
+
+export default handler;
